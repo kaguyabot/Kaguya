d6638c7871a12be76912a14d15ca9eaa689c3bb2
StageOsu <stageosu@gmail.com>
2019-06-11 01:23:17 -0400

    Re-enabled fun commands.

--- Kaguya/Modules/Fun/Fun.cs
+++ Kaguya/Modules/Fun/Fun.cs
@@ -50,7 +50,7 @@ namespace Kaguya.Modules.Fun
                 await BE(); return;
             }
 
-            foreach(var word in filteredWords)
+            foreach (var word in filteredWords)
             {
                 if (message.Contains(word))
                     return;
@@ -93,161 +93,161 @@ namespace Kaguya.Modules.Fun
             embed.WithTitle("Magic 8Ball");
             embed.WithDescription($"**{Context.User.Mention} {responses[num]}**");
             await BE();
-            
+
         }
 
-        //[Command("slap")]
-        //public async Task Slap(string target)
-        //{
-        //    var gif = await nekoClient.Action_v3.SlapGif();
-        //    embed.WithTitle($"{Context.User.Username} slaped {target}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("slap")]
+        public async Task Slap(string target)
+        {
+            var gif = await nekoClient.Action_v3.SlapGif();
+            embed.WithTitle($"{Context.User.Username} slaped {target}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("slap")]
-        //public async Task Slap(IGuildUser target)
-        //{
-        //    var gif = await nekoClient.Action_v3.SlapGif();
-        //    embed.WithTitle($"{Context.User.Username} slaped {target.Username}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("slap")]
+        public async Task Slap(IGuildUser target)
+        {
+            var gif = await nekoClient.Action_v3.SlapGif();
+            embed.WithTitle($"{Context.User.Username} slaped {target.Username}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("hug")]
-        //public async Task Hug(string target)
-        //{
-        //    var gif = await nekoClient.Action_v3.HugGif();
-        //    embed.WithTitle($"{Context.User.Username} hugged {target}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("hug")]
+        public async Task Hug(string target)
+        {
+            var gif = await nekoClient.Action_v3.HugGif();
+            embed.WithTitle($"{Context.User.Username} hugged {target}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("hug")]
-        //public async Task Hug(IGuildUser target)
-        //{
-        //    var gif = await nekoClient.Action_v3.HugGif();
-        //    embed.WithTitle($"{Context.User.Username} hugged {target.Username}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("hug")]
+        public async Task Hug(IGuildUser target)
+        {
+            var gif = await nekoClient.Action_v3.HugGif();
+            embed.WithTitle($"{Context.User.Username} hugged {target.Username}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("kiss")]
-        //public async Task Kiss(string target)
-        //{
-        //    var gif = await nekoClient.Action_v3.KissGif();
-        //    embed.WithTitle($"{Context.User.Username} kissed {target}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("kiss")]
+        public async Task Kiss(string target)
+        {
+            var gif = await nekoClient.Action_v3.KissGif();
+            embed.WithTitle($"{Context.User.Username} kissed {target}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("kiss")]
-        //public async Task Kiss(IGuildUser target)
-        //{
-        //    var gif = await nekoClient.Action_v3.KissGif();
-        //    embed.WithTitle($"{Context.User.Username} kissed {target.Username}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("kiss")]
+        public async Task Kiss(IGuildUser target)
+        {
+            var gif = await nekoClient.Action_v3.KissGif();
+            embed.WithTitle($"{Context.User.Username} kissed {target.Username}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("pat")]
-        //public async Task Pat(string target)
-        //{
-        //    var gif = await nekoClient.Action_v3.PatGif();
-        //    embed.WithTitle($"{Context.User.Username} patted {target}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("pat")]
+        public async Task Pat(string target)
+        {
+            var gif = await nekoClient.Action_v3.PatGif();
+            embed.WithTitle($"{Context.User.Username} patted {target}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("pat")]
-        //public async Task Pat(IGuildUser target)
-        //{
-        //    var gif = await nekoClient.Action_v3.PatGif();
-        //    embed.WithTitle($"{Context.User.Username} patted {target.Username}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("pat")]
+        public async Task Pat(IGuildUser target)
+        {
+            var gif = await nekoClient.Action_v3.PatGif();
+            embed.WithTitle($"{Context.User.Username} patted {target.Username}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("poke")]
-        //public async Task Poke(string target)
-        //{
-        //    var gif = await nekoClient.Action_v3.PokeGif();
-        //    embed.WithTitle($"{Context.User.Username} poked {target}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("poke")]
+        public async Task Poke(string target)
+        {
+            var gif = await nekoClient.Action_v3.PokeGif();
+            embed.WithTitle($"{Context.User.Username} poked {target}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("poke")]
-        //public async Task Poke(IGuildUser target)
-        //{
-        //    var gif = await nekoClient.Action_v3.PokeGif();
-        //    embed.WithTitle($"{Context.User.Username} poked {target.Username}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("poke")]
+        public async Task Poke(IGuildUser target)
+        {
+            var gif = await nekoClient.Action_v3.PokeGif();
+            embed.WithTitle($"{Context.User.Username} poked {target.Username}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("tickle")]
-        //public async Task Tickle(string target)
-        //{
-        //    var gif = await nekoClient.Action_v3.TickleGif();
-        //    embed.WithTitle($"{Context.User.Username} tickled {target}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("tickle")]
+        public async Task Tickle(string target)
+        {
+            var gif = await nekoClient.Action_v3.TickleGif();
+            embed.WithTitle($"{Context.User.Username} tickled {target}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("tickle")]
-        //public async Task Tickle(IGuildUser target)
-        //{
-        //    var gif = await nekoClient.Action_v3.TickleGif();
-        //    embed.WithTitle($"{Context.User.Username} tickled {target.Username}!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("tickle")]
+        public async Task Tickle(IGuildUser target)
+        {
+            var gif = await nekoClient.Action_v3.TickleGif();
+            embed.WithTitle($"{Context.User.Username} tickled {target.Username}!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("baka")]
-        //public async Task Baka()
-        //{
-        //    var gif = await nekoClient.Image_v3.BakaGif();
-        //    embed.WithTitle($"Baka!!");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("baka")]
+        public async Task Baka()
+        {
+            var gif = await nekoClient.Image_v3.BakaGif();
+            embed.WithTitle($"Baka!!");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("nekoavatar")]
-        //public async Task NekoAvatar()
-        //{
-        //    var gif = await nekoClient.Image_v3.NekoAvatar();
-        //    embed.WithTitle($"Neko Avatar for {Context.User.Username}");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("nekoavatar")]
+        public async Task NekoAvatar()
+        {
+            var gif = await nekoClient.Image_v3.NekoAvatar();
+            embed.WithTitle($"Neko Avatar for {Context.User.Username}");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("smug")]
-        //public async Task Smug()
-        //{
-        //    var gif = await nekoClient.Image_v3.SmugGif();
-        //    embed.WithTitle($"Smug（￣＾￣）");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("smug")]
+        public async Task Smug()
+        {
+            var gif = await nekoClient.Image_v3.SmugGif();
+            embed.WithTitle($"Smug（￣＾￣）");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("waifu")]
-        //public async Task Waifu()
-        //{
-        //    var gif = await nekoClient.Image_v3.Waifu();
-        //    embed.WithTitle($"Waifu (ﾉ≧ڡ≦)");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("waifu")]
+        public async Task Waifu()
+        {
+            var gif = await nekoClient.Image_v3.Waifu();
+            embed.WithTitle($"Waifu (ﾉ≧ڡ≦)");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
-        //[Command("wallpaper")]
-        //public async Task Wallpaper()
-        //{
-        //    var gif = await nekoClient.Image_v3.Wallpaper();
-        //    embed.WithTitle($"Wallpaper for {Context.User.Username}");
-        //    embed.WithImageUrl(gif.ImageUrl);
-        //    await BE();
-        //}
+        [Command("wallpaper")]
+        public async Task Wallpaper()
+        {
+            var gif = await nekoClient.Image_v3.Wallpaper();
+            embed.WithTitle($"Wallpaper for {Context.User.Username}");
+            embed.WithImageUrl(gif.ImageUrl);
+            await BE();
+        }
 
         private bool UserIsAdmin(SocketGuildUser user)
         {
