effa742b9579ca9bf021733bc19892c74197f428
CakeAndBanana <admin@cakeandbanana.nl>
2019-09-24 22:13:01 +0200

    Added User request builder. need to add processing.

---
+++ Kaguya/Core/Osu/Builder/OsuUserBuilder.cs
@@ -0,0 +1,48 @@
+﻿using Kaguya.Core.Osu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaguya.Core.Osu.Builder
+{
+    public class OsuUserBuilder : OsuBaseBuilder<OsuUserModel>
+    {
+        public string UserId; // u
+        public int Mode; // m
+
+        public OsuUserModel Execute()
+        {
+            var userArray = ExecuteJson(OsuRequest.User);
+            var user = ProcessJson(userArray);
+            return user;
+        }
+
+        public OsuUserModel ProcessJson(OsuUserModel[] userArray)
+        {
+            foreach (var item in userArray)
+            {
+                //Need processing
+            }
+
+            var userList = userArray.ToList();
+
+            return userList.FirstOrDefault();
+        }
+
+        public override string Build(StringBuilder urlBuilder)
+        {
+            if (!string.IsNullOrEmpty(UserId))
+            {
+                urlBuilder.Append("&u=").Append(UserId);
+            }
+
+            if (!string.IsNullOrEmpty(Mode.ToString()))
+            {
+                urlBuilder.Append("&m=").Append(Mode);
+            }
+            return urlBuilder.ToString();
+        }
+    }
+}
