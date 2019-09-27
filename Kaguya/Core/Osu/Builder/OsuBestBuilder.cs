f8187f36072ba4d8a8998125b5c8f10f9380ab82
CakeAndBanana <admin@cakeandbanana.nl>
2019-09-27 02:13:10 +0200

    Created BestBuilder and model.

---
+++ Kaguya/Core/Osu/Builder/OsuBestBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Kaguya.Core.Osu.Models;
+using System;
+using System.Text;
+
+namespace Kaguya.Core.Osu.Builder
+{
+    public class OsuBestBuilder : OsuBaseBuilder<OsuBestModel>
+    {
+        public string UserId; // u
+        public int Mode; // m
+        public int Limit; // limit
+
+        public override string Build(StringBuilder urlBuilder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
