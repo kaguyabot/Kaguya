6b299a211bb095f55e8f17094bf7e365d2f918a2
CakeAndBanana <admin@cakeandbanana.nl>
2019-09-28 00:17:32 +0200

    Added detail in model and builder

--- Kaguya/Core/Osu/Builder/OsuBestBuilder.cs
+++ Kaguya/Core/Osu/Builder/OsuBestBuilder.cs
@@ -1,5 +1,9 @@
 ﻿using Kaguya.Core.Osu.Models;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace Kaguya.Core.Osu.Builder
@@ -12,7 +16,67 @@ namespace Kaguya.Core.Osu.Builder
 
         public override string Build(StringBuilder urlBuilder)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrEmpty(UserId))
+            {
+                urlBuilder.Append("&u=").Append(UserId);
+            }
+
+            if (!string.IsNullOrEmpty(Mode.ToString()))
+            {
+                urlBuilder.Append("&m=").Append(Mode);
+            }
+
+            if (!string.IsNullOrEmpty(Limit.ToString()))
+            {
+                urlBuilder.Append("&limit=").Append(Limit);
+            }
+
+            return urlBuilder.ToString();
+        }
+
+        public OsuBestBuilder(string userid, int mode = 0, int limit = 5)
+        {
+            UserId = userid;
+            Mode = mode;
+            Limit = limit;
+        }
+
+        public List<OsuBestModel> Execute()
+        {
+            var recentArray = ExecuteJson(OsuRequest.BestPerformance);
+            recentArray = ProcessJson(recentArray);
+
+            return recentArray.ToList();
+        }
+
+        private OsuBestModel[] ProcessJson(OsuBestModel[] array)
+        {
+            int i = 1;
+            foreach (var item in array)
+            {
+                //Sets playnumber of this play.
+                item.play_number = i;
+
+                //Calculate accuracy for osu!standard
+                item.accuracy = OsuMisc.OsuAccuracy(item.count50, item.count100, item.count300, item.countmiss);
+
+                //Get string for mods
+                item.string_mods = OsuMisc.ModeNames(item.enabled_mods);
+
+                //Fill in Emote of Grade
+                item.rankemote = OsuMisc.OsuGrade(item.rank);
+
+                string mapBest = "";
+                using (WebClient client = new WebClient())
+                {
+                    mapBest = client.DownloadString($"https://osu.ppy.sh/api/get_beatmaps?k={Config.bot.OsuApiKey}&b={item.beatmap_id}");
+                }
+                item.beatmap = JsonConvert.DeserializeObject<OsuBeatmapModel[]>(mapBest)[0];
+
+                i++;
+            }
+
+            return array;
         }
     }
 }
--- Kaguya/Core/Osu/Models/OsuBestModel.cs
+++ Kaguya/Core/Osu/Models/OsuBestModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kaguya.Core.Osu.Models
 {
     public class OsuBestModel : OsuScoreableModel
     {
+        public int play_number { get; set; }
+        public int beatmap_id { get; set; }
+        public int score { get; set; }
+        public double starrating { get; set; }
+        public string maxcombo { get; set; }
+        public int perfect { get; set; }
+        public int enabled_mods { get; set; }
+        public string string_mods { get; set; }
+        public string user_id { get; set; }
+        public DateTime date { get; set; }
+        public string rank { get; set; }
+        public string rankemote { get; set; }
+        public double pp { get; set; }
+        public double accuracy { get; set; }
+        public OsuBeatmapModel beatmap { get; set; }
     }
 }
