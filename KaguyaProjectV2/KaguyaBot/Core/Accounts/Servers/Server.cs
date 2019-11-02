f7f1b3e3e834b32e815a8c6ab68139bdafee3caf
StageOsu <stageosu@gmail.com>
2019-11-02 02:43:44 -0400

    User.cs properties completed

---
+++ KaguyaProjectV2/KaguyaBot/Core/Accounts/Servers/Server.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaguyaProjectV2.KaguyaBot.Core.Accounts.Servers
+{
+    public class Server
+    {
+        uint ID { get; set; }
+    }
+}
---
+++ KaguyaProjectV2/KaguyaBot/Core/DataStorage/JsonStorage/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaguyaProjectV2.KaguyaBot.Core.DataStorage.JsonStorage
+{
+    public class Config
+    {
+
+    }
+}
