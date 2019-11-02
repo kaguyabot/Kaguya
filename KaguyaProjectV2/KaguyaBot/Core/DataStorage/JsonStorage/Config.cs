387d717af170c77b4ad73b18e8f8538d317343ad
StageOsu <stageosu@gmail.com>
2019-11-02 13:30:53 -0400

    Worked on Config class

--- KaguyaProjectV2/KaguyaBot/Core/DataStorage/JsonStorage/Config.cs
+++ KaguyaProjectV2/KaguyaBot/Core/DataStorage/JsonStorage/Config.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using Newtonsoft.Json;
+
+#region This file will load all Config file data into memory for the bot to use. This file contains very important credentials.
+#endregion
 
 namespace KaguyaProjectV2.KaguyaBot.Core.DataStorage.JsonStorage
 {
     public class Config
     {
+        string Token { get; set; }
+        string DefaultPrefix { get; set; }
+        string OsuApiKey { get; set; }
+        string TillerinoApiKey { get; set; }
+        string TopGGApiKey { get; set; }
+        string TopGGAuthorizationPassword { get; set; }
 
+        public Config()
+        {
+            Token = "";
+            DefaultPrefix = "$";
+            OsuApiKey = "";
+            TillerinoApiKey = "";
+            TopGGApiKey = "";
+            TopGGAuthorizationPassword = "";
+        }
+
+        public static void JsonDeserialization()
+        {
+            string filePath = $"{Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\"))}\\Release\\Resources\\config.json";
+            var json = JsonConvert.SerializeObject(filePath);
+
+
+        }
     }
 }
