b4191aa0a957899ec7bca2346f9e8ecaaec27d05
StageOsu <stageosu@gmail.com>
2019-11-02 02:54:50 -0400

    Started on Servers.cs and created an Interfaces folder.

--- KaguyaProjectV2/KaguyaBot/Core/Accounts/Servers/Server.cs
+++ KaguyaProjectV2/KaguyaBot/Core/Accounts/Servers/Server.cs
@@ -6,6 +6,11 @@ namespace KaguyaProjectV2.KaguyaBot.Core.Accounts.Servers
 {
     public class Server
     {
+        string Name { get; set; }
         uint ID { get; set; }
+        string CommandPrefix { get; set; }
+        Dictionary<ulong, long> MutedMembers { get; set; } //Dictionary<UserID, Duration>
+
+        //Continue tomorrow.
     }
 }
