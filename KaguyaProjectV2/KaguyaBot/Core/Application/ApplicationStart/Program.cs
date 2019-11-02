6f407142576aef58d172b7b8208adca425b9f5a9
StageOsu <stageosu@gmail.com>
2019-11-02 00:13:26 -0400

    Removed website project

---
+++ KaguyaProjectV2/KaguyaBot/Core/Application/ApplicationStart/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace KaguyaProjectV2.KaguyaBot.Core.Application.ApplicationStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
