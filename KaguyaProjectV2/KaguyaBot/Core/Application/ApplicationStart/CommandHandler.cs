80ff59c6f64d36b4c70558754b95dc85e35c11bf
StageOsu <stageosu@gmail.com>
2019-11-02 01:25:11 -0400

    Bot can now connect to the interwebs

---
+++ KaguyaProjectV2/KaguyaBot/Core/Application/ApplicationStart/CommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace KaguyaProjectV2.KaguyaBot.Core.Application.ApplicationStart
+{
+    public class CommandHandler
+    {
+        private readonly CommandService _commands;
+        private readonly DiscordShardedClient _client;
+        private readonly IServiceProvider _services;
+
+        public CommandHandler(IServiceProvider services)
+        {
+            _commands = services.GetRequiredService<CommandService>();
+            _client = services.GetRequiredService<DiscordShardedClient>();
+            _services = services;
+
+            _commands.CommandExecuted += CommandExecutedAsync;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        public async Task CommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        {
+            // command is unspecified when there was a search failure (command not found); we don't care about these errors
+            if (!command.IsSpecified)
+                return;
+
+            // the command was succesful, we don't care about this result, unless we want to log that a command succeeded.
+            if (result.IsSuccess)
+                return; //Log
+
+            // the command failed, let's notify the user that something happened.
+            await context.Channel.SendMessageAsync($"error: {result.ToString()}");
+        }
+
+    }
+}
--- KaguyaProjectV2/KaguyaBot/Core/Application/ApplicationStart/Program.cs
+++ KaguyaProjectV2/KaguyaBot/Core/Application/ApplicationStart/Program.cs
@@ -11,9 +11,48 @@ namespace KaguyaProjectV2.KaguyaBot.Core.Application.ApplicationStart
     class Program
     {
         static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            var config = new DiscordSocketConfig
+            {
+                TotalShards = 1
+            };
+
+            using (var services = ConfigureServices(config))
+            {
+                var client = services.GetRequiredService<DiscordShardedClient>();
+
+                client.ShardReady += OnReady;
+                client.Log += LogAsync;
+
+                await services.GetRequiredService<CommandHandler>().InitializeAsync();
+
+                await client.LoginAsync(TokenType.Bot, ""/* <---- TOKEN GOES HERE*/);
+                await client.StartAsync();
+
+                await Task.Delay(-1);
+            }
+        }
+
+        private ServiceProvider ConfigureServices(DiscordSocketConfig config)
+        {
+            return new ServiceCollection()
+                .AddSingleton(new DiscordShardedClient(config))
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandler>()
+                .BuildServiceProvider();
+        }
+
+        private async Task OnReady(DiscordSocketClient _client)
+        {
+            Console.WriteLine("Shard ready!");
+        }
+
+        private async Task LogAsync(LogMessage msg)
+        {
+            Console.WriteLine(msg);
         }
     }
 }
