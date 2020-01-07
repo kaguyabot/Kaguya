d4d8ad00db5a4ae65816f8612aa0bfb90f951487
stageosu <stageosu@gmail.com>
2020-01-07 12:23:54 +0100

    $poke

---
+++ KaguyaProjectV2/KaguyaBot/Core/Commands/Fun/Poke.cs
@@ -0,0 +1,51 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Humanizer;
+using KaguyaProjectV2.KaguyaBot.Core.Attributes;
+using KaguyaProjectV2.KaguyaBot.Core.Global;
+using KaguyaProjectV2.KaguyaBot.Core.KaguyaEmbed;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KaguyaProjectV2.KaguyaBot.Core.Commands.Fun
+{
+    public class Poke : ModuleBase<ShardedCommandContext>
+    {
+        [FunCommand]
+        [Command("Poke")]
+        [Summary("Poke somebody, or multiple people!")]
+        [Remarks("<user> {...}")]
+        public async Task Command(params SocketGuildUser[] users)
+        {
+            var pokeGif = await ConfigProperties.NekoClient.Action_v3.PokeGif();
+
+            if (users.Length == 1)
+            {
+                var embed = new KaguyaEmbedBuilder
+                {
+                    Title = $"Poke!",
+                    Description = $"{Context.User.Mention} poked {users[0].Mention}!",
+                    ImageUrl = pokeGif.ImageUrl
+                };
+
+                await ReplyAsync(embed: embed.Build());
+                return;
+            }
+            else
+            {
+                var names = new List<string>();
+                users.ToList().ForEach(x => names.Add(x.Mention));
+
+                var embed = new KaguyaEmbedBuilder
+                {
+                    Title = $"Poke!",
+                    Description = $"{Context.User.Mention} poked {names.Humanize()}!",
+                    ImageUrl = pokeGif.ImageUrl
+                };
+
+                await ReplyAsync(embed: embed.Build());
+            }
+        }
+    }
+}
