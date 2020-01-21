7eaac2e1794607f54e74b9def54016dff3806ef7
StageOsu <stageosu@gmail.com>
2020-01-20 20:49:06 -0500

    It's now possible to log all logtypes.

--- KaguyaProjectV2/KaguyaBot/Core/Commands/Administration/LogCommands/LogQuery.cs
+++ KaguyaProjectV2/KaguyaBot/Core/Commands/Administration/LogCommands/LogQuery.cs
@@ -20,8 +20,8 @@ namespace KaguyaProjectV2.KaguyaBot.Core.Commands.Administration.LogCommands
         /// <param name="enabled">A boolean stating whether we should enable the logging or disable it.</param>
         public static async Task<List<string>> LogSwitcher(string args, bool enabled, ulong guildId, SocketTextChannel channel = null)
         {
-            List<string> logTypes = ArrayInterpreter.ReturnParams(args).ToList();
-            Server server = await DatabaseQueries.GetOrCreateServerAsync(guildId);
+            var logTypes = ArrayInterpreter.ReturnParams(args).ToList();
+            var server = await DatabaseQueries.GetOrCreateServerAsync(guildId);
 
             foreach (var type in logTypes.ToList())
             {
@@ -46,28 +46,32 @@ namespace KaguyaProjectV2.KaguyaBot.Core.Commands.Administration.LogCommands
                         case "kicks": server.LogKicks = channel.Id; break;
                         case "bans": server.LogBans = channel.Id; break;
                         case "unbans": server.LogUnbans = channel.Id; break;
-                        case "uservoiceconnectionupdated": server.LogVoiceChannelConnections = channel.Id; break;
+                        case "voiceconnections": server.LogVoiceChannelConnections = channel.Id; break;
                         case "levelups": server.LogLevelAnnouncements = channel.Id; break;
                         case "fishlevels": server.LogFishLevels = channel.Id; break;
                         case "antiraid": server.LogAntiraids = channel.Id; break;
                         case "greetings": server.LogGreetings = channel.Id; break;
                         case "twitchnotifications": server.LogTwitchNotifications = channel.Id; break;
                         case "all":
-                            {
+                        {
+                            if (server.IsPremium)
                                 server.ModLog = channel.Id;
-                                server.LogDeletedMessages = channel.Id;
-                                server.LogUpdatedMessages = channel.Id;
-                                server.LogUserJoins = channel.Id;
-                                server.LogUserLeaves = channel.Id;
-                                server.LogAntiraids = channel.Id;
-                                server.LogKicks = channel.Id;
-                                server.LogBans = channel.Id;
-                                server.LogUnbans = channel.Id;
-                                server.LogFilteredPhrases = channel.Id;
-                                server.LogVoiceChannelConnections = channel.Id;
-                                server.LogLevelAnnouncements = channel.Id;
-                            }
-                            break;
+                            server.LogDeletedMessages = channel.Id;
+                            server.LogUpdatedMessages = channel.Id;
+                            server.LogFilteredPhrases = channel.Id;
+                            server.LogUserJoins = channel.Id;
+                            server.LogUserLeaves = channel.Id;
+                            server.LogKicks = channel.Id;
+                            server.LogBans = channel.Id;
+                            server.LogUnbans = channel.Id;
+                            server.LogVoiceChannelConnections = channel.Id;
+                            server.LogLevelAnnouncements = channel.Id;
+                            server.LogFishLevels = channel.Id;
+                            server.LogAntiraids = channel.Id;
+                            server.LogGreetings = channel.Id;
+                            server.LogTwitchNotifications = channel.Id;
+                        }
+                        break;
                         default: logTypes.Remove(type); break;
                     }
                 }
@@ -76,32 +80,39 @@ namespace KaguyaProjectV2.KaguyaBot.Core.Commands.Administration.LogCommands
                     await ConsoleLogger.LogAsync($"Server has disabled log type: [ID: {guildId} | Type: {type.ToUpperInvariant()}]", LogLvl.DEBUG);
                     switch (type.ToLower())
                     {
-                        case "kaguyaserverlog": server.ModLog = 0; break;
+                        case "modlog": server.ModLog = 0; break;
                         case "deletedmessages": server.LogDeletedMessages = 0; break;
                         case "updatedmessages": server.LogUpdatedMessages = 0; break;
+                        case "filteredphrases": server.LogFilteredPhrases = 0; break;
                         case "userjoins": server.LogUserJoins = 0; break;
                         case "userleaves": server.LogUserLeaves = 0; break;
-                        case "antiraid": server.LogAntiraids = 0; break;
                         case "kicks": server.LogKicks = 0; break;
                         case "bans": server.LogBans = 0; break;
                         case "unbans": server.LogUnbans = 0; break;
-                        case "filteredphrases": server.LogFilteredPhrases = 0; break;
-                        case "userconnectstovoice": server.LogVoiceChannelConnections = 0; break;
+                        case "voiceconnections": server.LogVoiceChannelConnections = 0; break;
                         case "levelups": server.LogLevelAnnouncements = 0; break;
+                        case "fishlevels": server.LogFishLevels = 0; break;
+                        case "antiraid": server.LogAntiraids = 0; break;
+                        case "greetings": server.LogGreetings = 0; break;
+                        case "twitchnotifications": server.LogTwitchNotifications = 0; break;
                         case "all":
                             {
-                                server.ModLog = 0;
+                                if (server.IsPremium)
+                                    server.ModLog = 0;
                                 server.LogDeletedMessages = 0;
                                 server.LogUpdatedMessages = 0;
+                                server.LogFilteredPhrases = 0;
                                 server.LogUserJoins = 0;
                                 server.LogUserLeaves = 0;
-                                server.LogAntiraids = 0;
                                 server.LogKicks = 0;
                                 server.LogBans = 0;
                                 server.LogUnbans = 0;
-                                server.LogFilteredPhrases = 0;
                                 server.LogVoiceChannelConnections = 0;
                                 server.LogLevelAnnouncements = 0;
+                                server.LogFishLevels = 0;
+                                server.LogAntiraids = 0;
+                                server.LogGreetings = 0;
+                                server.LogTwitchNotifications = 0;
                             }
                             break;
                         default: logTypes.Remove(type); break;
