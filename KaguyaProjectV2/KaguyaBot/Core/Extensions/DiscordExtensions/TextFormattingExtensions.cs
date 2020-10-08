19eeba737df97a0fb5184a15fad4769bf6c8dd7d
Harry <hburnett777@gmail.com>
2020-10-08 17:25:29 -0400

    Cleanup of TextFormattingExtensions.cs

--- KaguyaProjectV2/KaguyaBot/Core/Extensions/DiscordExtensions/TextFormattingExtensions.cs
+++ KaguyaProjectV2/KaguyaBot/Core/Extensions/DiscordExtensions/TextFormattingExtensions.cs
@@ -4,14 +4,11 @@ namespace KaguyaProjectV2.KaguyaBot.Core.Extensions.DiscordExtensions
 {
     public static class TextFormattingExtensions
     {
-        public static string ToDiscordBold(this string str)
-        {
-            return $"**{str}**";
-        }
-
-        public static string ToDiscordUnderlinedBold(this string str)
-        {
-            return $"__**{str}**__";
-        }
+        public static string ToDiscordBold(this string s) => $"**{s}**";
+        public static string ToDiscordUnderlined(this string s) => $"__{s}__";
+        public static string ToDiscordItalics(this string s) => $"*{s}*";
+        public static string ToDiscordUnderlinedBold(this string s) => $"__**{s}**__";
+        public static string ToDiscordBackticks(this string s) => $"`{s}`";
+        public static string ToDiscordCodeblock(this string s) => $"```{s}```";
     }
 }
