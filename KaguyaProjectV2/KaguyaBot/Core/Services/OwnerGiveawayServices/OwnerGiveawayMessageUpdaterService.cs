d0807a1cf8e7a1545487d542ce03da6eddc9540a
Harry <hburnett777@gmail.com>
2020-10-16 16:42:00 -0400

    Catch timeout exception

--- KaguyaProjectV2/KaguyaBot/Core/Services/OwnerGiveawayServices/OwnerGiveawayMessageUpdaterService.cs
+++ KaguyaProjectV2/KaguyaBot/Core/Services/OwnerGiveawayServices/OwnerGiveawayMessageUpdaterService.cs
@@ -67,46 +67,55 @@ namespace KaguyaProjectV2.KaguyaBot.Core.Services.OwnerGiveawayServices
                     
                     await userMessage.ModifyAsync(async m =>
                     {
-                        var cacheEmbed = userMessage.Embeds.FirstOrDefault();
-
-                        if (cacheEmbed == null)
+                        try
                         {
-                            await ConsoleLogger.LogAsync("The embed for an owner giveaway message was null!!",
-                                LogLvl.WARN);
-                            return;
+                                var cacheEmbed = userMessage.Embeds.FirstOrDefault();
+
+                            if (cacheEmbed == null)
+                            {
+                                await ConsoleLogger.LogAsync("The embed for an owner giveaway message was null!!",
+                                    LogLvl.WARN);
+                                return;
+                            }
+                            var embed = new KaguyaEmbedBuilder(EmbedColor.GOLD)
+                            {
+                                Title = cacheEmbed.Title,
+                                Description = cacheEmbed.Description
+                            };
+                        
+                            bool pointsGiveaway = giveaway.Points > 0;
+                            bool expGiveaway = giveaway.Exp > 0;
+                        
+                            var descSb = OwnerGiveawayCommand.DescriptionStringBuilder("1s", pointsGiveaway,
+                                expGiveaway,
+                                giveaway.Points, giveaway.Exp, out TimeSpan timeSpan);
+
+                            string lastLine = descSb.ToString().Split("\r\n")[^2];
+                            if (!giveaway.HasExpired && giveaway.Expiration < DateTime.Now.ToOADate() || 
+                                giveaway.Expiration < DateTime.Now.ToOADate())
+                            {
+                                descSb = descSb.Replace(lastLine, "This giveaway has ended.");
+
+                                giveaway.HasExpired = true;
+                                await DatabaseQueries.UpdateAsync(giveaway);
+                            }
+                            else
+                            {
+                                string humanizedTimeRemaining = (DateTime.FromOADate(giveaway.Expiration) - DateTime.Now).Humanize(2, minUnit: TimeUnit.Second);
+                                string endString = $"This giveaway will end in `{humanizedTimeRemaining}`.";
+                                
+                                descSb = descSb.Replace(lastLine, endString);
+                            }
+
+                            embed.Description = descSb.ToString();
+                            m.Embed = embed.Build();
                         }
-                        var embed = new KaguyaEmbedBuilder(EmbedColor.GOLD)
+                        catch (TimeoutException tException)
                         {
-                            Title = cacheEmbed.Title,
-                            Description = cacheEmbed.Description
-                        };
-                    
-                        bool pointsGiveaway = giveaway.Points > 0;
-                        bool expGiveaway = giveaway.Exp > 0;
-                    
-                        var descSb = OwnerGiveawayCommand.DescriptionStringBuilder("1s", pointsGiveaway,
-                            expGiveaway,
-                            giveaway.Points, giveaway.Exp, out TimeSpan timeSpan);
-
-                        string lastLine = descSb.ToString().Split("\r\n")[^2];
-                        if (!giveaway.HasExpired && giveaway.Expiration < DateTime.Now.ToOADate() || 
-                            giveaway.Expiration < DateTime.Now.ToOADate())
-                        {
-                            descSb = descSb.Replace(lastLine, "This giveaway has ended.");
-
-                            giveaway.HasExpired = true;
-                            await DatabaseQueries.UpdateAsync(giveaway);
+                            await ConsoleLogger.LogAsync(tException,
+                                "An unexpected timeout exception was thrown in the " +
+                                "owner giveaway message updater service.");
                         }
-                        else
-                        {
-                            string humanizedTimeRemaining = (DateTime.FromOADate(giveaway.Expiration) - DateTime.Now).Humanize(2, minUnit: TimeUnit.Second);
-                            string endString = $"This giveaway will end in `{humanizedTimeRemaining}`.";
-                            
-                            descSb = descSb.Replace(lastLine, endString);
-                        }
-
-                        embed.Description = descSb.ToString();
-                        m.Embed = embed.Build();
                     });
                 }
             };
