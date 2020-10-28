887ffb101af214291087ba6eeb8d12899cccf718
Harry Burnett <hburnett777@gmail.com>
2020-10-28 15:52:58 -0400

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -28,22 +28,22 @@ pipeline {
   stage('Build') {
    steps {
     dir("${env.WORKSPACE}/KaguyaProjectV2"){
-        cp "dotnet build -c Release"
+        sh "dotnet build -c Release"
     }
    }
   }
   stage('Copy External Dependencies'){
       steps{
           dir("${env.WORKSPACE}/KaguyaProjectV2/ExternalDependencies"){
-              sh "cp /Y Discord.Addons.Interactive.dll ${env.WORKSPACE}/KaguyaProjectV2/bin/Release/netcoreapp3.1/"
-              sh "cp /Y oppai.dll ${env.WORKSPACE}/KaguyaProjectV2/bin/Release/netcoreapp3.1/"
+              sh "cp -rf Discord.Addons.Interactive.dll ${env.WORKSPACE}/KaguyaProjectV2/bin/Release/netcoreapp3.1/"
+              sh "cp -rf oppai.dll ${env.WORKSPACE}/KaguyaProjectV2/bin/Release/netcoreapp3.1/"
           }
       }
   }
   stage('Start Bot + API') {
    steps {
     dir("${env.WORKSPACE}/KaguyaProjectV2/bin/Release/netcoreapp3.1"){
-        cp "dotnet KaguyaProjectV2.dll %Token% %Bot Owner ID% %Log Level% %Default Prefix% %osu! API Key% %Top.gg API Key% %MySQL Username% %MySQL Password% %MySQL Server% %MySQL Schema% %Twitch Client ID% %Twitch Authorization Token% %Danbooru Username% %Danbooru API Key% %Top.gg Webhook Port%"
+        sh "dotnet KaguyaProjectV2.dll %Token% %Bot Owner ID% %Log Level% %Default Prefix% %osu! API Key% %Top.gg API Key% %MySQL Username% %MySQL Password% %MySQL Server% %MySQL Schema% %Twitch Client ID% %Twitch Authorization Token% %Danbooru Username% %Danbooru API Key% %Top.gg Webhook Port%"
     }
    }
   }
