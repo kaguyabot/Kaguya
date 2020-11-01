07e4f78ad9cd032ed088bbd21b6f595326dcbae9
Harry <hburnett777@gmail.com>
2020-11-01 17:25:23 -0500

    v3.0 - Cancel running build if exists

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,21 @@
 pipeline {
  agent any
  stages {
+  stage('Cancel Existing Build if Exists') {
+  import hudson.model.Result
+  import jenkins.model.CauseOfInterruption
+  
+  //iterate through current project runs
+  build.getProject()._getRuns().iterator().each{ run ->
+    def exec = run.getExecutor()
+    //if the run is not a current build and it has executor (running) then stop it
+    if( run!=build && exec!=null ){
+        //prepare the cause of interruption
+        def cause = { "interrupted by build #${build.getId()}" as String } as CauseOfInterruption 
+        exec.interrupt(Result.ABORTED, cause)
+      }
+    }
+  }
   stage('Checkout') {
    steps {
      git branch: 'master', url: 'https://github.com/stageosu/Kaguya.git'
