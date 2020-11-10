5d1dc8f754821a1ae7131fbc7926df2c3339814f
Harry <hburnett777@gmail.com>
2020-11-10 09:46:54 -0500

    maintain 2 different jenkinsfiles.

---
+++ jenkinsfile-prod.groovy
@@ -0,0 +1,45 @@
+def buildNumber = env.BUILD_NUMBER as int
+if (buildNumber > 1) milestone(buildNumber - 1)
+milestone(buildNumber)
+
+pipeline {
+ agent any
+ stages {
+  stage('Checkout') {
+   steps {
+     git branch: 'master', url: 'https://github.com/stageosu/Kaguya.git'
+   }
+  }
+  stage('Restore Packages') {
+   steps {
+    dir("${env.WORKSPACE}/KaguyaProjectV2"){
+        sh "dotnet restore"
+    }
+   }
+  }
+  stage('Clean') {
+   steps {
+    dir("${env.WORKSPACE}/KaguyaProjectV2"){
+        sh "dotnet clean -c Release"
+      }
+    }
+  }
+  stage('Build') {
+   steps {
+    dir("${env.WORKSPACE}/KaguyaProjectV2"){
+        sh "echo killing any existing processes listening to port ${TopggWebhookPort}..."
+        sh "kill \$(lsof -t -i:${TopggWebhookPort}) || true" // Needed so that the web server dies each shutdown and doesn't leave an open port.
+        sh "echo all dotnet processes have been killed."
+        sh "dotnet build -c Release"
+    }
+   }
+  }
+  stage('Start Bot + API') {
+   steps {
+    dir("${env.WORKSPACE}/KaguyaProjectV2/bin/Release/netcoreapp3.1"){
+        sh "dotnet KaguyaProjectV2.dll ${Token} ${BotOwnerID} ${LogLevel} ${DefaultPrefix} ${osuAPIKey} ${TopggAPIKey} ${MySQLUsername} ${MySQLPassword} ${MySQLServer} ${MySQLSchema} ${TwitchClientID} ${TwitchAuthorizationToken} ${DanbooruUsername} ${DanbooruAPIKey} ${TopggWebhookPort}"
+    }
+   }
+  }
+ }
+}
