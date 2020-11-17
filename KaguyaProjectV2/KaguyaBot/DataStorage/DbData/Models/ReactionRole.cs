d16f141c7fa89b84c674a5811a230140657a9f37
Harry <hburnett777@gmail.com>
2020-11-16 20:26:12 -0500

    Updated ReactionRole.cs table mapping.

--- KaguyaProjectV2/KaguyaBot/DataStorage/DbData/Models/ReactionRole.cs
+++ KaguyaProjectV2/KaguyaBot/DataStorage/DbData/Models/ReactionRole.cs
@@ -4,27 +4,27 @@ using LinqToDB.Mapping;
 
 namespace KaguyaProjectV2.KaguyaBot.DataStorage.DbData.Models
 {
-    [Table(Name = "reactionroles")]
+    [Table(Name = "reaction_roles")]
     public class ReactionRole : IKaguyaQueryable<ReactionRole>, IServerSearchable<ReactionRole>
     {
         /// <summary>
         /// The ID of the role that this reaction role is assigned to.
         /// </summary>
-        [Column(Name = "roleid")]
+        [Column(Name = "role_id")]
         [NotNull]
         public ulong RoleId { get; set; }
 
         /// <summary>
         /// The ID of the message that this reaction role is attached to.
         /// </summary>
-        [Column(Name = "messageid")]
+        [Column(Name = "message_id")]
         [NotNull]
         public ulong MessageId { get; set; }
 
         /// <summary>
         /// The ID of the server in which this reaction role lives.
         /// </summary>
-        [Column(Name = "serverid")]
+        [Column(Name = "server_id")]
         [NotNull]
         public ulong ServerId { get; set; }
 
@@ -32,7 +32,7 @@ namespace KaguyaProjectV2.KaguyaBot.DataStorage.DbData.Models
         /// The ID of the emote that this reaction role is assigned to. If the reaction itself is
         /// actually a standard <see cref="Emoji"/>, we need to store its name instead. i.e. :joy: :beach:
         /// </summary>
-        [Column(Name = "emotenameid")]
+        [Column(Name = "emote_name_or_id")]
         [Nullable]
         public string EmoteNameorId { get; set; }
     }
