4a1513e009e72c9377a325abb7a9b1a83aeb3ca6
Harry <hburnett777@gmail.com>
2020-11-16 20:30:14 -0500

    Updated ServerRoleReward.cs table mapping.

--- KaguyaProjectV2/KaguyaBot/DataStorage/DbData/Models/ServerRoleReward.cs
+++ KaguyaProjectV2/KaguyaBot/DataStorage/DbData/Models/ServerRoleReward.cs
@@ -3,22 +3,22 @@ using LinqToDB.Mapping;
 
 namespace KaguyaProjectV2.KaguyaBot.DataStorage.DbData.Models
 {
-    [Table(Name = "serverrolerewards")]
+    [Table(Name = "server_role_rewards")]
     public class ServerRoleReward : IKaguyaQueryable<ServerRoleReward>, IServerSearchable<ServerRoleReward>
     {
-        [Column(Name = "ServerId")]
+        [Column(Name = "server_id")]
         [NotNull]
         public ulong ServerId { get; set; }
 
-        [Column(Name = "RoleId")]
+        [Column(Name = "role_id")]
         [NotNull]
         public ulong RoleId { get; set; }
 
-        [Column(Name = "Level")]
+        [Column(Name = "level")]
         [NotNull]
         public int Level { get; set; }
 
-        [Association(ThisKey = "ServerId", OtherKey = "ServerId")]
+        [Association(ThisKey = "server_id", OtherKey = "ServerId")]
         public Server Server { get; set; }
     }
 }
