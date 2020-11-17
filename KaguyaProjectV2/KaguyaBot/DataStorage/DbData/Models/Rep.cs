c9e44c519cec88f3838dbf87d129ab886dc01a84
Harry <hburnett777@gmail.com>
2020-11-16 20:27:16 -0500

    Updated Rep.cs table mapping.

--- KaguyaProjectV2/KaguyaBot/DataStorage/DbData/Models/Rep.cs
+++ KaguyaProjectV2/KaguyaBot/DataStorage/DbData/Models/Rep.cs
@@ -6,19 +6,19 @@ namespace KaguyaProjectV2.KaguyaBot.DataStorage.DbData.Models
     [Table(Name = "rep")]
     public class Rep : IKaguyaQueryable<Rep>, IKaguyaUnique<Rep>, IUserSearchable<Rep>
     {
-        [Column(Name = "UserId")]
+        [Column(Name = "user_id")]
         [NotNull]
         public ulong UserId { get; set; }
 
-        [Column(Name = "GivenBy")]
+        [Column(Name = "given_by")]
         [NotNull]
         public ulong GivenBy { get; set; }
 
-        [Column(Name = "TimeGiven")]
+        [Column(Name = "time_given")]
         [NotNull]
         public double TimeGiven { get; set; }
 
-        [Column(Name = "Reason")]
+        [Column(Name = "reason")]
         [NotNull]
         public string Reason { get; set; }
     }
