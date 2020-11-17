44f5f01ff3a6543c24677ef1049346c81f812461
Harry <hburnett777@gmail.com>
2020-11-16 20:22:40 -0500

    Updated MutedUser.cs table mapping.

--- KaguyaProjectV2/KaguyaBot/DataStorage/DbData/Models/MutedUser.cs
+++ KaguyaProjectV2/KaguyaBot/DataStorage/DbData/Models/MutedUser.cs
@@ -9,31 +9,31 @@ namespace KaguyaProjectV2.KaguyaBot.DataStorage.DbData.Models
         IUserSearchable<MutedUser>,
         IKaguyaUnique<MutedUser>
     {
-        [Column(Name = "ServerId")]
+        [Column(Name = "server_id")]
         [NotNull]
         public ulong ServerId { get; set; }
 
-        [Column(Name = "UserId")]
+        [Column(Name = "user_id")]
         [NotNull]
         public ulong UserId { get; set; }
 
         /// <summary>
         /// When the mute will expire, in OADate form.
         /// </summary>
-        [Column(Name = "ExpiresAt")]
+        [Column(Name = "expires_at")]
         [NotNull]
         public double ExpiresAt { get; set; }
 
         /// <summary>
         /// FK_KaguyaServer_MutedUsers
         /// </summary>
-        [Association(ThisKey = "ServerId", OtherKey = "Id", CanBeNull = false)]
+        [Association(ThisKey = "server_id", OtherKey = "id", CanBeNull = false)]
         public Server Server { get; set; }
 
         /// <summary>
         /// FK_KaguyaUser_MutedUsers
         /// </summary>
-        [Association(ThisKey = "UserId", OtherKey = "Id", CanBeNull = false)]
+        [Association(ThisKey = "user_id", OtherKey = "id", CanBeNull = false)]
         public User User { get; set; }
     }
 }
