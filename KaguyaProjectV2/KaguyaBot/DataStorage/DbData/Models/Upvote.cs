55f346e2d436d8f0a411c002070cbe18a26c3083
Harry <hburnett777@gmail.com>
2020-11-16 20:57:46 -0500

    Updated Upvote.cs table mapping.

--- KaguyaProjectV2/KaguyaBot/DataStorage/DbData/Models/Upvote.cs
+++ KaguyaProjectV2/KaguyaBot/DataStorage/DbData/Models/Upvote.cs
@@ -6,55 +6,56 @@ namespace KaguyaProjectV2.KaguyaBot.DataStorage.DbData.Models
     [Table(Name = "upvotes")]
     public class TopGgWebhook : IKaguyaQueryable<TopGgWebhook>, IKaguyaUnique<TopGgWebhook>, IUserSearchable<TopGgWebhook>
     {
-        [PrimaryKey]
+        // todo: Duplicate of KaguyaProjectV2.KaguyaApi.Database.Models.TopGgWebhook.cs ??
+        [Column(Name = "vote_id")]
         public string VoteId { get; set; }
 
         /// <summary>
         /// ID of the bot that received a vote
         /// </summary>
-        [Column(Name = "BotId")]
+        [Column(Name = "bot_id")]
         [NotNull]
         public ulong BotId { get; set; }
 
         /// <summary>
         /// ID of the user who voted
         /// </summary>
-        [Column(Name = "UserId")]
+        [Column(Name = "user_id")]
         [NotNull]
         public ulong UserId { get; set; }
 
         /// <summary>
         /// The time the user upvoted, in OADate form.
         /// </summary>
-        [Column(Name = "Time")]
+        [Column(Name = "time_voted")]
         [NotNull]
         public double TimeVoted { get; set; }
 
         /// <summary>
         /// The type of the vote (should always be "upvote" except when using the test button it's "test")
         /// </summary>
-        [Column(Name = "VoteType")]
+        [Column(Name = "vote_type")]
         [NotNull]
         public string UpvoteType { get; set; }
 
         /// <summary>
         /// Whether the weekend multiplier is in effect, meaning users' votes count as two
         /// </summary>
-        [Column(Name = "IsWeekend")]
+        [Column(Name = "is_weekend")]
         [NotNull]
         public bool IsWeekend { get; set; }
 
         /// <summary>
         /// Query string params found on the /bot/:ID/vote page. Example: ?a=1&b=2
         /// </summary>
-        [Column(Name = "QueryParams")]
+        [Column(Name = "query_params")]
         [NotNull]
         public string QueryParams { get; set; }
 
         /// <summary>
         /// Whether or not a reminder has been sent out for this upvote object.
         /// </summary>
-        [Column(Name = "ReminderSent")]
+        [Column(Name = "reminder_sent")]
         [NotNull]
         public bool ReminderSent { get; set; }
     }
