17aaaf256de91cf2ff8753f5bbe61e2a3d409bc3
Harry <hburnett777@gmail.com>
2020-12-29 20:06:47 -0500

    Update jenkinsfile-open-beta.groovy

--- jenkinsfile-open-beta.groovy
+++ jenkinsfile-open-beta.groovy
@@ -1,11 +1,23 @@
 pipeline {
     agent any
     stages {
-        stage('Git Checkout') {
+        stage('Build') {
             steps {
                 sh "docker build -t kaguya:${BUILD_NUMBER} ."
                 sh "docker tag kaguya:${BUILD_NUMBER} kaguya:latest" 
             }
-        } 
+        }
+        stage('Stop Old') {
+            // todo: Try-catch
+            sh "docker stop kaguya"
+            sh "docker rm kaguya"
+        }
+        stage('Deploy') {
+            steps {
+                sh "docker build -t kaguya:${BUILD_NUMBER} ."
+                sh "docker tag kaguya:${BUILD_NUMBER} kaguya:latest"
+                sh "docker run --name=kaguya --restart=always -v /etc/kaguya/appsettings.json:/KaguyaApp/appsettings.json -d kaguya:latest"
+            }
+        }
     }
 }
