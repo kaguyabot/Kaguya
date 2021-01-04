cfe13a350a75aa774fa4dcb24bbb79647db2e2d9
Harry <hburnett777@gmail.com>
2021-01-04 08:24:16 -0500

    ShardsLoggedIn global property

--- Kaguya/Kaguya/Global.cs
+++ Kaguya/Kaguya/Global.cs
@@ -17,9 +17,20 @@ namespace Kaguya
         public const string InviteUrl = "https://discord.com/oauth2/authorize?client_id=538910393918160916&scope=bot&permissions=536341759";
         public static readonly string Version = "v4.0-beta-" + Process.GetCurrentProcess().StartTime.ToShortDateString().Replace('/', '.');
 
+        public static int ShardsLoggedIn { get; private set; }
+        
+        /// <summary>
+        /// Gets the current uptime for the program in a user-friendly readable format.
+        /// </summary>
+        /// <returns></returns>
         public static string GetUptimeString()
         {
             return (DateTime.Now - Process.GetCurrentProcess().StartTime).Humanize(2, minUnit: TimeUnit.Second, maxUnit: TimeUnit.Day);
         }
+
+        /// <summary>
+        /// Increases the number of logged in shards by 1.
+        /// </summary>
+        public static void IncrementLoggedInShards() => ShardsLoggedIn++;
     }
 }
