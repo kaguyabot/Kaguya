843942f048607c46e506ebdc57950a131faf092b
Harry <hburnett777@gmail.com>
2021-01-04 16:27:46 -0500

    Logging now features a timestamp

--- Kaguya/Kaguya/Program.cs
+++ Kaguya/Kaguya/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+#pragma warning disable 618
 
 namespace Kaguya
 {
@@ -17,8 +19,15 @@ namespace Kaguya
 	        CreateHostBuilder(args).Build().Run();
         }
 
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        private static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddSimpleConsole(opts =>
+                    {
+                        opts.TimestampFormat = "[MM-dd-yyyy HH:mm:ss:fff] ";
+                    });
+                })
                 .ConfigureAppConfiguration(builder =>
                 {
                     builder.AddEnvironmentVariables(prefix: "Kaguya_");
