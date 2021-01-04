cc1df627779b0f646cd1352518993fa7c420d1ed
Harry <hburnett777@gmail.com>
2021-01-04 17:03:18 -0500

    KaguyaEmbedBuilder can now parse colors from hexadecimal.

--- Kaguya/Kaguya/Discord/KaguyaEmbedBuilder.cs
+++ Kaguya/Kaguya/Discord/KaguyaEmbedBuilder.cs
@@ -1,10 +1,22 @@
 ﻿using Discord;
+using Kaguya.Discord.DiscordExtensions;
+using Kaguya.Exceptions;
 
 namespace Kaguya.Discord
 {
     public class KaguyaEmbedBuilder : EmbedBuilder
     {
-        public KaguyaEmbedBuilder() => new KaguyaEmbedBuilder(global::Discord.Color.Blue);
+        public KaguyaEmbedBuilder() { this.Color ??= global::Discord.Color.Blue; }
+        /// <summary>
+        /// Converts the given hexadecimal color string into a <see cref="Color"/>.
+        ///
+        /// Example: new KaguyaEmbedBuilder("FFFF99") { }
+        /// </summary>
+        /// <param name="hex"></param>
+        /// <exception cref="ColorParseException">Thrown if the hexadecimal string could not be parsed as a uint.</exception>
+        public KaguyaEmbedBuilder(string hex) : this(new Color(hex.ToHexadecimal()))
+        { }
+
         public KaguyaEmbedBuilder(Color color)
         {
             this.Color = color;
