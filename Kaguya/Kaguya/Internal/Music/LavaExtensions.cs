b74108544ee41c108a6bba6342e08d64f5cdbb83
Harry <hburnett777@gmail.com>
2021-01-13 09:15:08 -0500

    (WIP) $search command

---
+++ Kaguya/Kaguya/Internal/Music/LavaExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Victoria;
+
+namespace Kaguya.Internal.Music
+{
+    public static class LavaExtensions
+    {
+        public static async Task<bool> SafeJoinAsync(this LavaNode lavaNode, SocketUser user, ISocketMessageChannel channel)
+        {
+            if (user is not SocketGuildUser guildUser || channel is not ITextChannel textChannel)
+            {
+                return false;
+            }
+            
+            IVoiceChannel voiceChannel = ((IVoiceState) guildUser).VoiceChannel;
+            if (voiceChannel == null)
+            {
+                return false;
+            }
+
+            if (!lavaNode.HasPlayer(guildUser.Guild))
+            {
+                await lavaNode.JoinAsync(voiceChannel, textChannel);
+            }
+
+            return true;
+        }
+    }
+}
