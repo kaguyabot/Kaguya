da4eff457f8afdb8615e8fb3898b27b217dc6231
Harry <hburnett777@gmail.com>
2021-01-22 16:45:36 -0500

    Beta invite URL (Global.cs)

--- Kaguya/Kaguya/Global.cs
+++ Kaguya/Kaguya/Global.cs
@@ -16,6 +16,7 @@ namespace Kaguya
         public const string StoreLink = "[Kaguya Premium](" + StoreUrl + ")";
         public const string SupportDiscordUrl = "https://discord.gg/aumCJhr";
         public const string InviteUrl = "https://discord.com/oauth2/authorize?client_id=538910393918160916&scope=bot&permissions=536341759";
+        public const string BetaInviteUrl = "https://discord.com/oauth2/authorize?client_id=367403886841036820&scope=bot&permissions=1610083583";
         public const string DiscordTermsLink = "[Terms of Service](https://discord.com/terms)";
         public const string DiscordCommunityGuidelinesLink = "[Community Guidelines](https://discord.com/guidelines)";
         
