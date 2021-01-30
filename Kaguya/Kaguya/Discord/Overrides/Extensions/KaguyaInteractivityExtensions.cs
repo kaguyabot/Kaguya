384497089b4ad9c1b77b4e7335cc88dcf54d1e9a
Harry <hburnett777@gmail.com>
2021-01-30 15:35:20 -0500

    Custom implementation of reactionselection

---
+++ Kaguya/Kaguya/Discord/Overrides/Extensions/KaguyaInteractivityExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Qommon.Collections;
+
+namespace Kaguya.Discord.Overrides.Extensions
+{
+    internal static partial class KaguyaInteractivityExtensions
+    {
+        public static ReadOnlyCollection<T> AsReadOnlyCollection<T>(this IEnumerable<T> collection)
+            => new ReadOnlyCollection<T>(collection.ToArray());
+
+        public static ReadOnlyDictionary<TKey, TValue> AsReadOnlyDictionary<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
+            => new ReadOnlyDictionary<TKey, TValue>(dictionary);
+    }
+}
