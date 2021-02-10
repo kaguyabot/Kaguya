4a333ce7faf905f2f0b9c3112912685661c9405c
Harry <hburnett777@gmail.com>
2021-02-10 05:50:43 -0500

    Rename text channel command

--- Kaguya/Kaguya/Discord/Commands/Administration/TextChannel.cs
+++ Kaguya/Kaguya/Discord/Commands/Administration/TextChannel.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Kaguya.Discord.DiscordExtensions;
 using Kaguya.Internal.Attributes;
 using Kaguya.Internal.Enums;
 using Microsoft.Extensions.Logging;
@@ -17,11 +18,8 @@ namespace Kaguya.Discord.Commands.Administration
     {
         private readonly ILogger<TextChannel> _logger;
         private const int MAX_LENGTH = 100;
-        
-        private static readonly char[] _invalidChars =
-        {
-            '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', ' '
-        };
+
+        private static readonly char[] _invalidChars = "123456789abcdefghijklmnopqrstuvwxyz".ToCharArray();
 
         public TextChannel(ILogger<TextChannel> logger) : base(logger) { _logger = logger; }
 
@@ -34,28 +32,14 @@ namespace Kaguya.Discord.Commands.Administration
         [Example("e x a m p l e")]
         public async Task CreateTextChannelCommandAsync([Remainder]string name)
         {
-            name = name.Replace(" ", "-");
-
-            if (name.Length > MAX_LENGTH)
+            if (!IsValidTextChannelName(name, out string error))
             {
-                await SendBasicErrorEmbedAsync("The maximum amount of characters in a " +
-                                               "channel name is 100.");
-
+                await SendBasicErrorEmbedAsync(error);
                 return;
             }
 
-            foreach (char c in _invalidChars)
-            {
-                if (name.Contains(c))
-                {
-                    await SendBasicErrorEmbedAsync("Your channel name contains invalid characters. " +
-                                                   "Please type just the channel name, like this: " +
-                                                   "`my channel` or `my-channel`.");
-
-                    return;
-                }
-            }
-
+            ReplaceSpaces(ref name);
+            
             try
             {
                 await Context.Guild.CreateTextChannelAsync(name);
@@ -89,6 +73,68 @@ namespace Kaguya.Discord.Commands.Administration
             }
 
             await SendBasicSuccessEmbedAsync("Successfully deleted the text channel.");
+        }
+
+        [Command("-rename")]
+        [Alias("-r")]
+        [Summary("Renames the text channel to the desired name.")]
+        [Remarks("<channel> <new name>")]
+        [Example("#my-channel my new name")]
+        public async Task RenameTextChannelCommandAsync(ITextChannel channel, [Remainder]string newName)
+        {
+            if (!IsValidTextChannelName(newName, out string error))
+            {
+                await SendBasicErrorEmbedAsync(error);
+                return;
+            }
+            
+            ReplaceSpaces(ref newName);
+
+            try
+            {
+                await channel.ModifyAsync(x => x.Name = newName);
+            }
+            catch (Exception e)
+            {
+                await SendBasicErrorEmbedAsync($"Failed to rename text channel. Error:\n{e.Message}");
+                _logger.LogDebug(e, $"Failed to rename text channel in guild {Context.Guild.Id}.");
+            }
+
+            await SendBasicSuccessEmbedAsync($"Renamed " + $"#{channel.Name}".AsBold() + $" to {newName.AsBold()}");
+        }
+        
+        private static bool IsValidTextChannelName(string name, out string error)
+        {
+            ReplaceSpaces(ref name);
+
+            if (name.Length > MAX_LENGTH)
+            {
+                error = "The maximum amount of characters in a " +
+                        "channel name is 100.";
+                
+                return false;
+            }
+
+            foreach (char c in _invalidChars)
+            {
+                if (!name.Contains(c))
+                {
+                    error = "Your channel name contains invalid characters. " +
+                            "Please type the new channel name, like this: " +
+                            "`my channel` or `my-channel`. Only alphanumeric latin characters " +
+                            "are permitted (no symbols).";
+
+                    return false;
+                }
+            }
+
+            error = null;
+            return true;
+        }
+        
+        private static void ReplaceSpaces(ref string channelName)
+        {
+            channelName = channelName.Replace(' ', '-');
         }
     }
 }
