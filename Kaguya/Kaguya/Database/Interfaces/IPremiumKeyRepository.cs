3fbe0547936218292a704008f127b34c7ffbdb12
Harry <hburnett777@gmail.com>
2021-02-11 14:13:41 -0500

    PremiumKeyRepository now accepts a creatorId paramater

--- Kaguya/Kaguya/Database/Interfaces/IPremiumKeyRepository.cs
+++ Kaguya/Kaguya/Database/Interfaces/IPremiumKeyRepository.cs
@@ -10,15 +10,17 @@ namespace Kaguya.Database.Interfaces
         /// <summary>
         /// Generates and inserts a single key with the specified duration.
         /// </summary>
-        /// <param name="duration"></param>
+        /// <param name="duration">How long the key should last for once it is redeemed.</param>
         /// <returns></returns>
         public Task GenerateAndInsertAsync(TimeSpan duration);
+
         /// <summary>
         /// Generates and inserts a collection of keys with the specified duration.
         /// </summary>
-        /// <param name="amount"></param>
-        /// <param name="duration"></param>
+        /// <param name="amount">The amount of keys to generate and insert</param>
+        /// <param name="creatorId">The Discord user ID of the person who is creating the keys (typically the bot owner)</param>
+        /// <param name="duration">How long each of these keys should last for once they're redeemed.</param>
         /// <returns></returns>
-        public Task<IList<PremiumKey>> GenerateAndInsertAsync(int amount, TimeSpan duration);
+        public Task<IList<PremiumKey>> GenerateAndInsertAsync(int amount, ulong creatorId, TimeSpan duration);
     }
 }
