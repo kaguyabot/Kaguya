e4d82fb020a0899278bac9e7f77c8d3459684881
Harry <hburnett777@gmail.com>
2021-02-11 19:27:15 -0500

    Additional premium fix migrations

---
+++ Kaguya/Kaguya/Migrations/20210212001322_premiumFix.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kaguya.Migrations
+{
+    public partial class premiumFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PremiumKeys",
+                table: "PremiumKeys");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PremiumKeys",
+                table: "PremiumKeys",
+                columns: new[] { "Id", "Key" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PremiumKeys",
+                table: "PremiumKeys");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PremiumKeys",
+                table: "PremiumKeys",
+                column: "Key");
+        }
+    }
+}
