3778b2a2284c852097b62b152b5b2fd72e76d5fb
Harry <hburnett777@gmail.com>
2021-02-11 19:26:00 -0500

    NSFW migrations

---
+++ Kaguya/Kaguya/Migrations/20210212000958_kaguya_server_nsfw.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kaguya.Migrations
+{
+    public partial class kaguya_server_nsfw : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNsfwAllowed",
+                table: "KaguyaServers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "NsfwAllowanceTime",
+                table: "KaguyaServers",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<ulong>(
+                name: "NsfwAllowedId",
+                table: "KaguyaServers",
+                type: "bigint unsigned",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsNsfwAllowed",
+                table: "KaguyaServers");
+
+            migrationBuilder.DropColumn(
+                name: "NsfwAllowanceTime",
+                table: "KaguyaServers");
+
+            migrationBuilder.DropColumn(
+                name: "NsfwAllowedId",
+                table: "KaguyaServers");
+        }
+    }
+}
