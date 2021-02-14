36824ad97529d33a67723a1bdce630bd9a0f3dab
Harry <hburnett777@gmail.com>
2021-02-14 12:01:19 -0500

    StatisticsUploaderWorker is now working correctly

--- Kaguya/Kaguya/Internal/Services/Recurring/StatisticsUploaderWorker.cs
+++ Kaguya/Kaguya/Internal/Services/Recurring/StatisticsUploaderWorker.cs
@@ -4,38 +4,44 @@ using System.Threading.Tasks;
 using Kaguya.Database.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Kaguya.Internal.Services.Recurring
 {
     public class StatisticsUploaderWorker : BackgroundService, ITimerReceiver
     {
+        private readonly ILogger<StatisticsUploaderWorker> _logger;
         private readonly ITimerService _timerService;
         private readonly IServiceProvider _serviceProvider;
 
-        public StatisticsUploaderWorker(ITimerService timerService, IServiceProvider serviceProvider)
+        public StatisticsUploaderWorker(ILogger<StatisticsUploaderWorker> logger, ITimerService timerService, IServiceProvider serviceProvider)
         {
+            _logger = logger;
             _timerService = timerService;
             _serviceProvider = serviceProvider;
         }
         
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            // First execution
-            await _timerService.TriggerAtAsync(DateTime.Now.AddMinutes(1), this);
-
-            if (!stoppingToken.IsCancellationRequested)
+            if (stoppingToken.IsCancellationRequested)
             {
-                using (var scope = _serviceProvider.CreateScope())
-                {
-                    var statsRepo = scope.ServiceProvider.GetRequiredService<KaguyaStatisticsRepository>();
-                    await statsRepo.PostNewAsync();
-                }
+                return;
             }
+            
+            await _timerService.TriggerAtAsync(DateTime.Now, this);
         }
-
+        
         public async Task HandleTimer(object payload)
         {
             await _timerService.TriggerAtAsync(DateTime.Now.AddMinutes(1), this);
+
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var statsRepo = scope.ServiceProvider.GetRequiredService<KaguyaStatisticsRepository>();
+                await statsRepo.PostNewAsync();
+                
+                _logger.LogDebug("Fresh statistics posted");
+            }
         }
     }
 }
