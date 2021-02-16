08a84550a9869d12114958d9f31bc72818597177
Harry <hburnett777@gmail.com>
2021-02-16 02:14:01 -0500

    stats migrations

---
+++ Kaguya/Kaguya/Migrations/20210215235610_user-lastvoted.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kaguya.Migrations
+{
+    public partial class userlastvoted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpvoted",
+                table: "KaguyaUsers",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpvoted",
+                table: "KaguyaUsers");
+        }
+    }
+}
---
+++ Kaguya/Kaguya/Migrations/20210216065300_stats_connectedservers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kaguya.Migrations
+{
+    public partial class stats_connectedservers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ConnectedServers",
+                table: "KaguyaStatistics",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConnectedServers",
+                table: "KaguyaStatistics");
+        }
+    }
+}
