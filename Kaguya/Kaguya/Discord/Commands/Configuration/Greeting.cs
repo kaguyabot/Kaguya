4c61a347a3435ea1f5fd7adbc4d04db2f48acc3c
Harry <hburnett777@gmail.com>
2021-02-21 18:02:36 -0500

    Implemented $greeting -view and $greeting -toggle

--- Kaguya/Kaguya/Discord/Commands/Configuration/Greeting.cs
+++ Kaguya/Kaguya/Discord/Commands/Configuration/Greeting.cs
@@ -6,6 +6,7 @@ using Discord.Commands;
 using Interactivity;
 using Interactivity.Confirmation;
 using Kaguya.Database.Repositories;
+using Kaguya.Discord.Overrides.Extensions;
 using Kaguya.Internal.Attributes;
 using Kaguya.Internal.Enums;
 using Kaguya.Internal.Extensions.DiscordExtensions;
@@ -15,8 +16,9 @@ namespace Kaguya.Discord.Commands.Configuration
 {
     [Module(CommandModule.Configuration)]
     [Group("greeting")]
+    [Alias("greet")]
     [RequireUserPermission(GuildPermission.Administrator)]
-    [RequireUserPermission(GuildPermission.SendMessages)]
+    [RequireBotPermission(GuildPermission.SendMessages)]
     public class Greeting : KaguyaBase<Greeting>
     {
         private readonly ILogger<Greeting> _logger;
@@ -34,11 +36,14 @@ namespace Kaguya.Discord.Commands.Configuration
         [Command("-setmsg", RunMode = RunMode.Async)]
         [Summary("Allows you to set the custom greeting message for your server. This message " +
                  "will be displayed to new people when they join your server for the first time. " +
-                 "Optional paramaterized options may also be specified in your greeting message.\n\n" +
+                 "Optional paramaterized options may also be specified in your greeting message. " +
+                 "Paramaters must be in all caps, otherwise they will not work.\n\n" +
                  "__**Message Parameters:**__\n" +
                  "- `{USERMENTION}` -> Mentions the user\n" +
-                 "- `{MEMBERCOUNT}` -> The count of members in your server, formatted as `1st`, `2nd`, `3rd`, `4th`\n")]
+                 "- `{MEMBERCOUNT}` -> The count of members in your server, formatted as `1st`, `2nd`, `3rd`, `4th`\n" +
+                 "- `{SERVERNAME}` -> The name of this server")]
         [Remarks("<message>")]
+        [Example("Welcome {USERMENTION} to {SERVERNAME}, you are the {MEMBERCOUNT} member!", ExampleStringFormat.CodeblockMultiLine)]
         public async Task GreetingSetMessageCommandAsync([Remainder] string message)
         {
             if (message.Length > 1750)
@@ -48,6 +53,7 @@ namespace Kaguya.Discord.Commands.Configuration
 
                 return;
             }
+
             var server = await _serverRepository.GetOrCreateAsync(Context.Guild.Id);
 
             server.CustomGreeting = message;
@@ -120,15 +126,59 @@ namespace Kaguya.Discord.Commands.Configuration
                     _interactivityService.DelayedSendMessageAndDeleteAsync(Context.Channel, 
                         deleteDelay: TimeSpan.FromSeconds(15), embed: embed.Build());
                 }
+
+                await _serverRepository.UpdateAsync(server);
             }
         }
         
-        [Command("-setchannel")]
+        [Command("-setchannel", RunMode = RunMode.Async)]
         [Summary("Sets the channel for where greetings will be sent.")]
         [Remarks("<message>")] // Delete if no remarks needed.
         public async Task GreetingSetChannelCommandAsync(ITextChannel textChannel)
         {
+            var server = await _serverRepository.GetOrCreateAsync(Context.Guild.Id);
+
+            ulong? oldChannel = server.CustomGreetingTextChannelId;
+            bool hadPrevious = oldChannel.HasValue;
             
+            server.CustomGreetingTextChannelId = textChannel.Id;
+            await _serverRepository.UpdateAsync(server);
+
+            string response = $"Linked greetings to {textChannel.Mention}";
+
+            if (hadPrevious && oldChannel.Value != Context.Channel.Id)
+            {
+                var oldTextChannel = Context.Guild.GetTextChannel(oldChannel.Value);
+
+                if (oldTextChannel != null)
+                {
+                    response += $" (was {oldTextChannel.Mention})";
+                }
+            }
+            
+            await SendBasicSuccessEmbedAsync(response);
+
+            if (!server.CustomGreetingIsEnabled)
+            {
+                var toggleEmbed = GetBasicEmbedBuilder("I see that your greetings are disabled.\n" +
+                                          "Would you like to turn them on?".AsBold(), KaguyaColors.LightYellow);
+
+                var result = await _interactivityService.SendConfirmationAsync(toggleEmbed, Context.Channel, TimeSpan.FromMinutes(5));
+
+                if (result.IsSuccess)
+                {
+                    server.CustomGreetingIsEnabled = true;
+                    await _serverRepository.UpdateAsync(server);
+                    
+                    await SendBasicSuccessEmbedAsync("Greeting messages enabled. They will be sent in " +
+                                                     $"{textChannel.Mention} when new members join the server.");
+                }
+                else if (result.IsCancelled)
+                {
+                    await SendBasicSuccessEmbedAsync("Greetings will remain disabled. Enable them with the " +
+                                                     $"`{server.CommandPrefix}greeting -toggle` command.");
+                }
+            }
         }
         
         [Command("-clearmsg")]
@@ -137,7 +187,15 @@ namespace Kaguya.Discord.Commands.Configuration
         [Remarks("<message>")] // Delete if no remarks needed.
         public async Task GreetingClearMessageCommandAsync()
         {
+            var server = await _serverRepository.GetOrCreateAsync(Context.Guild.Id);
+
+            server.CustomGreeting = null;
+            server.CustomGreetingIsEnabled = false;
+
+            await _serverRepository.UpdateAsync(server);
             
+            await SendBasicSuccessEmbedAsync("The custom greeting message has been cleared. Greetings " +
+                                           "have been disabled.");
         }
         
         [Command("-toggle")]
@@ -145,7 +203,25 @@ namespace Kaguya.Discord.Commands.Configuration
         [Remarks("<message>")] // Delete if no remarks needed.
         public async Task GreetingToggleCommandAsync()
         {
+            var server = await _serverRepository.GetOrCreateAsync(Context.Guild.Id);
+            server.CustomGreetingIsEnabled = !server.CustomGreetingIsEnabled;
+
+            bool isNowEnabled = server.CustomGreetingIsEnabled;
             
+            await _serverRepository.UpdateAsync(server);
+
+            string state = isNowEnabled ? "enabled" : "disabled";
+            await SendBasicSuccessEmbedAsync($"Successfully {state} greetings.\n" +
+                                             $"Use `{server.CommandPrefix}greeting -view` to view " +
+                                             $"the current message.");
+
+            if (isNowEnabled && String.IsNullOrWhiteSpace(server.CustomGreeting))
+            {
+                await SendBasicEmbedAsync("It looks like your greetings are enabled " +
+                                          "but you don't have a greeting message set.\n\n" +
+                                          "In order for greetings to work, you must set the " +
+                                          $"message via `{server.CommandPrefix}greeting -setmsg`.", KaguyaColors.LightYellow);
+            }
         }
         
         [Command("-view")]
@@ -153,7 +229,18 @@ namespace Kaguya.Discord.Commands.Configuration
         [Remarks("<message>")] // Delete if no remarks needed.
         public async Task GreetingViewCommandAsync()
         {
-            
+            var server = await _serverRepository.GetOrCreateAsync(Context.Guild.Id);
+            var msg = server.CustomGreeting;
+
+            if (String.IsNullOrWhiteSpace(msg))
+            {
+                await SendBasicErrorEmbedAsync("You haven't set up a message. Create one with " +
+                                               $"`{server.CommandPrefix}greeting -setmsg`.");
+
+                return;
+            }
+
+            await SendBasicEmbedAsync($"Current Greeting:".AsBoldUnderlined() + $"\n```{msg}```", KaguyaColors.Tan, false);
         }
     }
 }
