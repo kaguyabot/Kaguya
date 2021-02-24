a8ed10c269ad06e368286c5977e6112ff9e19a42
Harry <hburnett777@gmail.com>
2021-02-24 08:52:59 -0500

    remove "IsCurrentlyPurgingMessages"

--- Kaguya/Kaguya/Migrations/20210223151116_Server_RemoveIsCurrentlyPurgingMessages.Designer.cs
+++ Kaguya/Kaguya/Migrations/20210223151116_Server_RemoveIsCurrentlyPurgingMessages.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Kaguya.Database.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Kaguya.Migrations
 {
     [DbContext(typeof(KaguyaDbContext))]
-    partial class KaguyaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210223151116_Server_RemoveIsCurrentlyPurgingMessages")]
+    partial class Server_RemoveIsCurrentlyPurgingMessages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
