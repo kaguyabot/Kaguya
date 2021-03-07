fdb6d3a665f553b4dc63759f0075f717506e1ab0
Harry <hburnett777@gmail.com>
2021-03-07 15:58:20 -0500

    Examples added to $clear

--- Kaguya/Kaguya/Discord/Commands/Administration/Clear.cs
+++ Kaguya/Kaguya/Discord/Commands/Administration/Clear.cs
@@ -28,16 +28,13 @@ namespace Kaguya.Discord.Commands.Administration
         }
 
         [Command(RunMode = RunMode.Async)]
-        public async Task ClearRecentCommand(SocketGuildUser user = null)
-        {
-            await ClearRecentCommand(10, user);
-        }
-
-        [Command(RunMode = RunMode.Async)]
         [Summary("Deletes the most recent number of messages specified in the current channel, up to 100. When " +
                  "used without arguments, the 10 most recent messages are removed. " +
                  "Cannot delete messages that are older than two weeks. Specify a user to only clear that user's messages.")]
         [Remarks("[amount] [user]")]
+        [Example("50")]
+        [Example("@user")]
+        [Example("50 @user")]
         public async Task ClearRecentCommand(int amount = 10, SocketGuildUser user = null)
         {
             if (amount < 1 || amount > 100)
@@ -83,6 +80,12 @@ namespace Kaguya.Discord.Commands.Administration
 
             await Task.Delay(5000);
             Internal.Memory.ServersCurrentlyPurgingMessages.TryRemove(Context.Guild.Id, out var _);
+        }
+        
+        [Command(RunMode = RunMode.Async)]
+        public async Task ClearRecentCommand(SocketGuildUser user = null)
+        {
+            await ClearRecentCommand(10, user);
         }
     }
 }
