008d6d9d9ca31842350879145ca4893a9cbfc3af
Harry <hburnett777@gmail.com>
2021-03-23 14:15:00 -0400

    Updated version string to v4.5.1

--- Kaguya/Kaguya/Global.cs
+++ Kaguya/Kaguya/Global.cs
@@ -27,7 +27,7 @@ namespace Kaguya
 		public const string WikiChangelog = "https://github.com/kaguyabot/Kaguya/wiki/Changelog";
 		public const string DiscordTermsLink = "[Terms of Service](https://discord.com/terms)";
 		public const string DiscordCommunityGuidelinesLink = "[Community Guidelines](https://discord.com/guidelines)";
-		public static readonly string Version = "4.5";
+		public static readonly string Version = "4.5.1";
 		/// <summary>
 		///  The number of shards currently logged into Discord.
 		/// </summary>
